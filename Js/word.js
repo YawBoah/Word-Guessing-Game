a193abca4ec412c217da1252f2e0903395c0c06f
YBoah <yawboahene.yb@gmail.com>
2023-09-30 01:43:32 +0000

    More words and hint

--- Js/word.js
+++ Js/word.js
@@ -195,4 +195,204 @@ const wordList = [
         word: "rain",
         hint: "related to a water"
     },
+    {
+        word: "Dubai",
+        hint: "Developed country name"
+    },
+    {
+        word: "Photo",
+        hint: "Representation of person or scene"
+    },
+    {
+        word: "Nile",
+        hint: "Largest river in the world"
+    },
+    {
+        word: "Rain",
+        hint: "Related to water"
+    },
+    {
+        word: "Apple",
+        hint: "A popular fruit and tech company"
+    },
+    {
+        word: "Ocean",
+        hint: "Vast bodies of saltwater on Earth"
+    },
+    {
+        word: "Guitar",
+        hint: "A musical instrument with strings"
+    },
+    {
+        word: "Sunshine",
+        hint: "Bright and warm sunlight"
+    },
+    {
+        word: "Elephant",
+        hint: "A large, gray mammal with a trunk"
+    },
+    {
+        word: "Adventure",
+        hint: "Exciting and daring experiences"
+    },
+    {
+        word: "Chocolate",
+        hint: "Sweet, brown treat made from cacao"
+    },
+    {
+        word: "Galaxy",
+        hint: "A massive system of stars and planets"
+    },
+    {
+        word: "Friendship",
+        hint: "Close and supportive relationships"
+    },
+    {
+        word: "Butterfly",
+        hint: "Colorful insect with delicate wings"
+    },
+    {
+        word: "Mountain",
+        hint: "Tall landforms with peaks"
+    },
+    {
+        word: "Laughter",
+        hint: "The sound of joy and amusement"
+    },
+    {
+        word: "Bicycle",
+        hint: "Human-powered vehicle with two wheels"
+    },
+    {
+        word: "Moonlight",
+        hint: "Illumination from the moon"
+    },
+    {
+        word: "Camera",
+        hint: "Device for capturing photographs"
+    },
+    {
+        word: "Forest",
+        hint: "Dense, wooded area with trees"
+    },
+    {
+        word: "Harmony",
+        hint: "Pleasant and balanced combination"
+    },
+    {
+        word: "Island",
+        hint: "Landmass surrounded by water"
+    },
+    {
+        word: "Serenade",
+        hint: "Musical performance for someone"
+    },
+    {
+        word: "Rainbow",
+        hint: "Colorful arc in the sky after rain"
+    },
+    {
+        word: "Bookshelf",
+        hint: "Furniture for storing books"
+    },
+    {
+        word: "Adventure",
+        hint: "Exciting and daring experiences"
+    },
+    {
+        word: "Beach",
+        hint: "Sandy shore by the sea"
+    },
+    {
+        word: "Coffee",
+        hint: "Popular caffeinated beverage"
+    },
+    {
+        word: "Panda",
+        hint: "Large black and white bear"
+    },
+    {
+        word: "Starfish",
+        hint: "Marine creature with radial arms"
+    },
+    {
+        word: "Symphony",
+        hint: "Elaborate musical composition"
+    },
+    {
+        word: "Treasure",
+        hint: "Valuable items hidden or buried"
+    },
+    {
+        word: "Waterfall",
+        hint: "Flowing water from a height"
+    },
+    {
+        word: "Fireworks",
+        hint: "Explosive displays of light"
+    },
+    {
+        word: "Sunrise",
+        hint: "Dawn and the start of the day"
+    },
+    {
+        word: "Soccer",
+        hint: "Popular ball sport"
+    },
+    {
+        word: "Whisper",
+        hint: "Soft spoken words"
+    },
+    {
+        word: "Castle",
+        hint: "Fortified building with turrets"
+    },
+    {
+        word: "Rainforest",
+        hint: "Lush, tropical woodland with rainfall"
+    },
+    {
+        word: "Adventure",
+        hint: "Exciting and daring experiences"
+    },
+    {
+        word: "Sunrise",
+        hint: "Dawn and the start of the day"
+    },
+    {
+        word: "Telescope",
+        hint: "Optical instrument for distant viewing"
+    },
+    {
+        word: "Butterfly",
+        hint: "Colorful insect with delicate wings"
+    },
+    {
+        word: "Moonlight",
+        hint: "Illumination from the moon"
+    },
+    {
+        word: "Symphony",
+        hint: "Elaborate musical composition"
+    },
+    {
+        word: "Laughter",
+        hint: "The sound of joy and amusement"
+    },
+    {
+        word: "Chocolate",
+        hint: "Sweet, brown treat made from cacao"
+    },
+    {
+        word: "Waterfall",
+        hint: "Flowing water from a height"
+    },
+    {
+        word: "Starfish",
+        hint: "Marine creature with radial arms"
+    },
+    {
+        word: "Treasure",
+        hint: "Valuable items hidden or buried"
+    },
 ]
