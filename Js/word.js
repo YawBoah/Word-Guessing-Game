5b53e70c78f26fda4b27c7e72803df7b7a58b1ff
YBoah <yawboahene.yb@gmail.com>
2023-09-30 02:37:38 +0000

    More words

--- Js/word.js
+++ Js/word.js
@@ -1,110 +1,266 @@
 const wordList = [
     {
-        word: "Sailboat",
+        word: "accra",
+        hint: "Capital city of Ghana"
+    },
+    {
+        word: "ashanti",
+        hint: "Ethnic group in Ghana"
+    },
+    {
+        word: "cocoa",
+        hint: "Important crop and export of Ghana"
+    },
+    {
+        word: "kente",
+        hint: "Traditional woven fabric in Ghana"
+    },
+    {
+        word: "gold coast",
+        hint: "Former name of Ghana due to gold resources"
+    },
+    {
+        word: "ghanaian cedi",
+        hint: "Currency of Ghana"
+    },
+    {
+        word: "independence",
+        hint: "Ghana gained independence from British colonial rule in 1957"
+    },
+    {
+        word: "ghana black stars",
+        hint: "National football (soccer) team of Ghana"
+    },
+    {
+        word: "adinkra symbols",
+        hint: "Traditional symbols representing concepts in Ghanaian culture"
+    },
+    {
+        word: "fufu",
+        hint: "Popular Ghanaian staple food made from cassava and plantains"
+    },
+    {
+        word: "cape coast castle",
+        hint: "Historic slave trading fort in Ghana"
+    },
+    {
+        word: "asante stool",
+        hint: "Symbol of Ashanti unity and power"
+    },
+    {
+        word: "akosombo dam",
+        hint: "Hydropower dam on the Volta River in Ghana"
+    },
+    {
+        word: "akan",
+        hint: "Ethnic group in Ghana known for their culture and language"
+    },
+    {
+        word: "ghanaian jollof rice",
+        hint: "Delicious rice dish with a Ghanaian twist"
+    },
+    {
+        word: "citi fm",
+        hint: "Popular radio station in Ghana"
+    },
+    {
+        word: "lake volta",
+        hint: "Largest man-made lake in the world by surface area, located in Ghana"
+    },
+    {
+        word: "kwame nkrumah",
+        hint: "First President of Ghana and a key figure in the country's independence movement"
+    },
+    {
+        word: "adomi bridge",
+        hint: "Iconic bridge spanning the Volta River in Ghana"
+    },
+    {
+        word: "ghanaian highlife music",
+        hint: "Popular genre of music blending African and Western influences"
+    },
+    {
+        word: "ghanaian hospitality",
+        hint: "Warm and welcoming culture of the Ghanaian people"
+    },
+    {
+        word: "cedi beads",
+        hint: "Traditional Ghanaian beads made from glass or ceramic"
+    },
+    {
+        word: "makola market",
+        hint: "Famous market in Accra, Ghana"
+    },
+    {
+        word: "nkrumah mausoleum",
+        hint: "Memorial dedicated to Kwame Nkrumah in Accra"
+    },
+    {
+        word: "ghanaian kente weaving",
+        hint: "Art of weaving colorful and intricate Kente cloth"
+    },
+    {
+        word: "w.e.b. du bois center",
+        hint: "Historical site honoring the civil rights activist in Accra"
+    },
+    {
+        word: "ghanaian films",
+        hint: "Ghana's growing film industry known as 'Ghallywood'"
+    },
+    {
+        word: "cocoa swollen shoot virus",
+        hint: "Disease affecting cocoa plants in Ghana"
+    },
+    {
+        word: "ghanaian bead-making",
+        hint: "Craftsmanship in creating intricate bead designs"
+    },
+    {
+        word: "cocoa board of ghana",
+        hint: "Government agency overseeing cocoa production"
+    },
+    {
+        word: "ghanaian dance",
+        hint: "Energetic and rhythmic traditional dance forms"
+    },
+    {
+        word: "cape three points",
+        hint: "Southernmost point in Ghana"
+    },
+    {
+        word: "ghanaian sankofa bird",
+        hint: "Symbolizing learning from the past in Akan culture"
+    },
+    {
+        word: "ghanaian fufu pounder",
+        hint: "Traditional utensil used to pound fufu"
+    },
+    {
+        word: "ghanaian art",
+        hint: "Vibrant and diverse art scene in Ghana"
+    },
+    {
+        word: "ghanaian proverbs",
+        hint: "Wise sayings reflecting cultural values and wisdom"
+    },
+    {
+        word: "ghanaian handicrafts",
+        hint: "Traditional crafts like woodcarving and pottery"
+    },
+    {
+        word: "kwahu mountains",
+        hint: "Scenic mountain range in Ghana"
+    },
+    {
+        word: "ghanaian textiles",
+        hint: "Variety of colorful fabrics used for clothing"
+    },
+    {
+        word: "sailboat",
         hint: "Boat propelled by sails"
     },
     {
-        word: "Cherry",
+        word: "cherry",
         hint: "Small, red fruit"
     },
     {
-        word: "Lighthouse",
+        word: "lighthouse",
         hint: "Tall tower with a light to guide ships"
     },
     {
-        word: "Canyon",
+        word: "canyon",
         hint: "Deep, narrow valley with steep sides"
     },
     {
-        word: "Penguin",
+        word: "penguin",
         hint: "Flightless, black and white bird"
     },
     {
-        word: "Rainforest",
+        word: "rainforest",
         hint: "Lush, tropical woodland with rainfall"
     },
     {
-        word: "Candle",
+        word: "candle",
         hint: "Wax stick with a wick for lighting"
     },
     {
-        word: "Dragon",
+        word: "dragon",
         hint: "Mythical fire-breathing creature"
     },
     {
-        word: "Cappuccino",
+        word: "cappuccino",
         hint: "Espresso-based coffee with steamed milk"
     },
     {
-        word: "Serenade",
+        word: "serenade",
         hint: "Musical performance, often romantic"
     },
     {
-        word: "Rainbow",
+        word: "rainbow",
         hint: "Colorful arc in the sky after rain"
     },
     {
-        word: "Desert",
+        word: "desert",
         hint: "Dry, arid landscape with little rainfall"
     },
     {
-        word: "Tiger",
+        word: "tiger",
         hint: "Large, striped carnivorous feline"
     },
     {
-        word: "Carousel",
+        word: "carousel",
         hint: "Merry-go-round amusement ride"
     },
     {
-        word: "Dolphin",
+        word: "dolphin",
         hint: "Marine mammal known for its intelligence"
     },
     {
-        word: "Orchestra",
+        word: "orchestra",
         hint: "Large ensemble of musicians"
     },
     {
-        word: "Harbor",
+        word: "harbor",
         hint: "Sheltered area for ships"
     },
     {
-        word: "Astronaut",
+        word: "astronaut",
         hint: "Person trained for space travel"
     },
     {
-        word: "Cottage",
+        word: "cottage",
         hint: "Small, cozy country house"
     },
     {
-        word: "Palm Tree",
+        word: "palm Tree",
         hint: "Tropical tree with feather-like leaves"
     },
     {
-        word: "Peacock",
+        word: "peacock",
         hint: "Colorful and elegant bird"
     },
     {
-        word: "Waterfall",
+        word: "waterfall",
         hint: "Flowing water from a height"
     },
     {
-        word: "Cave",
+        word: "cave",
         hint: "Natural underground chamber"
     },
     {
-        word: "Sushi",
+        word: "sushi",
         hint: "Japanese dish with vinegared rice and seafood"
     },
     {
-        word: "Journey",
+        word: "journey",
         hint: "Adventure or travel from one place to another"
     },
     {
-        word: "Carnival",
+        word: "carnival",
         hint: "Festive event with games and rides"
     },
     {
-        word: "Pirate",
+        word: "pirate",
         hint: "Seafarer who engages in robbery and plunder"
     },
     {
@@ -292,209 +448,213 @@ const wordList = [
         hint: "Developed country name"
     },
     {
-        word: "Photo",
+        word: "photo",
         hint: "Representation of person or scene"
     },
     {
-        word: "Nile",
+        word: "nile",
         hint: "Largest river in the world"
     },
     {
-        word: "Rain",
+        word: "nain",
         hint: "Related to water"
     },
     {
-        word: "Apple",
+        word: "apple",
         hint: "A popular fruit and tech company"
     },
     {
-        word: "Ocean",
+        word: "ocean",
         hint: "Vast bodies of saltwater on Earth"
     },
     {
-        word: "Sunshine",
+        word: "sunshine",
         hint: "Bright and warm sunlight"
     },
     {
-        word: "Elephant",
+        word: "elephant",
         hint: "A large, gray mammal with a trunk"
     },
     {
-        word: "Adventure",
+        word: "adventure",
         hint: "Exciting and daring experiences"
     },
     {
-        word: "Chocolate",
+        word: "chocolate",
         hint: "Sweet, brown treat made from cacao"
     },
     {
-        word: "Galaxy",
+        word: "galaxy",
         hint: "A massive system of stars and planets"
     },
     {
-        word: "Friendship",
+        word: "friendship",
         hint: "Close and supportive relationships"
     },
     {
-        word: "Butterfly",
+        word: "butterfly",
         hint: "Colorful insect with delicate wings"
     },
     {
-        word: "Mountain",
+        word: "mountain",
         hint: "Tall landforms with peaks"
     },
     {
-        word: "Laughter",
+        word: "laughter",
         hint: "The sound of joy and amusement"
     },
     {
-        word: "Bicycle",
+        word: "bicycle",
         hint: "Human-powered vehicle with two wheels"
     },
     {
-        word: "Moonlight",
+        word: "moonlight",
         hint: "Illumination from the moon"
     },
     {
-        word: "Camera",
+        word: "camera",
         hint: "Device for capturing photographs"
     },
     {
-        word: "Forest",
+        word: "forest",
         hint: "Dense, wooded area with trees"
     },
     {
-        word: "Harmony",
+        word: "harmony",
         hint: "Pleasant and balanced combination"
     },
     {
-        word: "Island",
+        word: "island",
         hint: "Landmass surrounded by water"
     },
     {
-        word: "Serenade",
+        word: "serenade",
         hint: "Musical performance for someone"
     },
     {
-        word: "Rainbow",
+        word: "rainbow",
         hint: "Colorful arc in the sky after rain"
     },
     {
-        word: "Bookshelf",
+        word: "bookshelf",
         hint: "Furniture for storing books"
     },
     {
-        word: "Beach",
+        word: "beach",
         hint: "Sandy shore by the sea"
     },
     {
-        word: "Coffee",
+        word: "coffee",
         hint: "Popular caffeinated beverage"
     },
     {
-        word: "Panda",
+        word: "panda",
         hint: "Large black and white bear"
     },
     {
-        word: "Starfish",
+        word: "starfish",
         hint: "Marine creature with radial arms"
     },
     {
-        word: "Symphony",
+        word: "symphony",
         hint: "Elaborate musical composition"
     },
     {
-        word: "Treasure",
+        word: "treasure",
         hint: "Valuable items hidden or buried"
     },
     {
-        word: "Waterfall",
+        word: "waterfall",
         hint: "Flowing water from a height"
     },
     {
-        word: "Fireworks",
+        word: "fireworks",
         hint: "Explosive displays of light"
     },
     {
-        word: "Sunrise",
+        word: "sunrise",
         hint: "Dawn and the start of the day"
     },
     {
-        word: "Soccer",
+        word: "football",
         hint: "Popular ball sport"
     },
     {
-        word: "Whisper",
+        word: "whisper",
         hint: "Soft spoken words"
     },
     {
-        word: "Castle",
+        word: "castle",
         hint: "Fortified building with turrets"
     },
     {
-        word: "Rainforest",
+        word: "rainforest",
         hint: "Lush, tropical woodland with rainfall"
     },
     {
-        word: "Camping",
+        word: "camping",
         hint: "Outdoor recreational activity"
     },
     {
-        word: "Eclipse",
+        word: "eclipse",
         hint: "Obscuring of one celestial body by another"
     },
     {
-        word: "Flower",
+        word: "flower",
         hint: "Colorful and fragrant plant bloom"
     },
     {
-        word: "Jungle",
+        word: "jungle",
         hint: "Wild and dense tropical forest"
     },
     {
-        word: "Kite",
+        word: "kite",
         hint: "Flying object on a string"
     },
     {
-        word: "Lake",
+        word: "lake",
         hint: "Inland body of water"
     },
     {
-        word: "Nature",
+        word: "nature",
         hint: "The natural world and its phenomena"
     },
     {
-        word: "Paradise",
+        word: "paradise",
         hint: "A perfect and idyllic place"
     },
     {
-        word: "Quicksilver",
+        word: "quicksilver",
         hint: "Another name for the element mercury"
     },
     {
-        word: "Thunderstorm",
+        word: "thunderstorm",
         hint: "Storm with lightning, thunder, and rain"
     },
     {
-        word: "Umbrella",
+        word: "umbrella",
         hint: "Protection from rain and sun"
     },
     {
-        word: "Volcano",
+        word: "volcano",
         hint: "Mountain that erupts with lava and ash"
     },
     {
-        word: "Xylophone",
+        word: "xylophone",
         hint: "Musical instrument with wooden bars"
     },
     {
-        word: "Yacht",
+        word: "yacht",
         hint: "Luxury boat for sailing or cruising"
     },
     {
-        word: "Zebra",
+        word: "zebra",
         hint: "A black and white striped African animal"
     },
+    {
+        word:"yaw",
+        hint:"A male person born on Thursday in Ghana"
+    }
 ];
 
 
